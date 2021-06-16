5b728d9b8b2b9ad21d8138b277bc2688be6c3145
madeleineostoja <madi@madeleineostoja.com>
2021-06-16 17:12:07 +1200

    fix storybook

--- template/.storybook/main.cjs
+++ template/.storybook/main.cjs
@@ -1,6 +1,8 @@
 const preprocess = require('svelte-preprocess');
+const path = require('path');
 
 module.exports = {
+  core: { builder: 'storybook-builder-vite' },
   stories: [
     '../src/**/*.stories.mdx',
     '../src/**/*.stories.@(js|jsx|ts|tsx|svelte)'
@@ -11,5 +13,17 @@ module.exports = {
       postcss: true,
       defaults: { style: 'postcss', script: 'typescript' }
     })
+  },
+  async viteFinal(config) {
+    config.resolve.alias = {
+      $app: path.resolve('./.svelte-kit/dev/runtime/app'),
+      $src: path.resolve('./src'),
+      $types: path.resolve('./@types')
+    };
+
+    // Allow files outside root for any aliased modules
+    config.server.fsServe.strict = false;
+
+    return config;
   }
 };
